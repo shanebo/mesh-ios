165d33bf2799a2ced48af9ba7454ab3ea30b981b
Joe Osburn <joe@jnodev.com>
2017-10-22 20:14:37 -0500

    Initial Commit

---
+++ mesh-ios/mesh_ios.h
@@ -0,0 +1,19 @@
+//
+//  mesh_ios.h
+//  mesh-ios
+//
+//  Created by Joe Osburn on 10/22/17.
+//  Copyright © 2017 Mesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mesh_ios.
+FOUNDATION_EXPORT double mesh_iosVersionNumber;
+
+//! Project version string for mesh_ios.
+FOUNDATION_EXPORT const unsigned char mesh_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mesh_ios/PublicHeader.h>
+
+
